4394d2fc575d53d3b04a3ae0f830e574bbd4034b
Rune Ibsen <rui@copenhagensoftware.com>
2018-10-13 09:57:42 +0200

    Add two simple micro services

---
+++ GreetingMs/Controllers/GreetingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace UserMs.Controllers
+{
+    public class GreetingController : Controller
+    {
+        [Route("greeting")]
+        public async Task<IActionResult> Get()
+        {
+            var client = new HttpClient();
+            var s = await client.GetStringAsync("http://localhost:52219/user");
+
+            return Ok("Hello " + s);
+        }
+    }
+}
